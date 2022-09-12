d2fa3678428accfade9793e8d363576d47ff92d0
agvan <agvandyke25@gmail.com>
2022-09-12 13:29:55 -0500

    created the type definitiion

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,8 @@
+// imports  the gql tagged template function
+const { gql } = require("apollo-server-express");
+
+// creates the typeDefs
+const typeDefs = gql``;
+
+// export the typeDefs
+module.exports = typeDefs;
