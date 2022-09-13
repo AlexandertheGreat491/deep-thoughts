1c36b9d40356104e500e52f8c3b3a12031e5a03e
agvan <agvandyke25@gmail.com>
2022-09-13 13:29:44 -0500

    updated mutation to make the query work

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,20 +1,20 @@
-const { User, Thought } = require('../models');
+const { User, Thought } = require("../models");
 
 const resolvers = {
   Query: {
     // get all users
     users: async () => {
       return User.find()
-        .select('-__v -password')
-        .populate('thoughts')
-        .populate('friends');
+        .select("-__v -password")
+        .populate("thoughts")
+        .populate("friends");
     },
     // get a user by username
     user: async (parent, { username }) => {
       return User.findOne({ username })
-        .select('-__v -password')
-        .populate('friends')
-        .populate('thoughts');
+        .select("-__v -password")
+        .populate("friends")
+        .populate("thoughts");
     },
     thoughts: async (parent, { username }) => {
       const params = username ? { username } : {};
@@ -22,8 +22,17 @@ const resolvers = {
     },
     thought: async (parent, { _id }) => {
       return Thought.findOne({ _id });
-    }
-  }
+    },
+  },
+  // resolver for login() and addUser() mutations
+  Mutation: {
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+
+      return user;
+    },
+    login: async () => {},
+  },
 };
 
 module.exports = resolvers;
