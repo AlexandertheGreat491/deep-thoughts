df1a6e01512a3f997d1976ad5fffcc399be95b4e
agvan <agvandyke25@gmail.com>
2022-09-13 14:24:21 -0500

    mutations updated to sign a token and return an object that combines the token with the user's data

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -33,8 +33,9 @@ const resolvers = {
   Mutation: {
     addUser: async (parent, args) => {
       const user = await User.create(args);
+      const token = signToken(user);
 
-      return user;
+      return { token, user };
     },
     login: async (parent, { email, password }) => {
       const user = await User.findOne({ email });
@@ -49,7 +50,8 @@ const resolvers = {
         throw new AuthenticationError("Incorrect credentials");
       }
 
-      return user;
+      const token = signToken(user);
+      return { token, user };
     },
   },
 };
