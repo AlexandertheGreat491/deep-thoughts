d2540f3c1ad31815b059f6c35db950799b510b8d
agvan <agvandyke25@gmail.com>
2022-09-13 14:49:18 -0500

    added the authMiddleware method to the module.exports

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -5,11 +5,40 @@ const jwt = require('jsonwebtoken');
 const secret = 'mysecretsshhhhh';
 const expiration = '2h';
 
+
 module.exports = {
     // the signToken() function expects user object & will add that user's username, email, & id properties to the token.
     signToken: function({username, email, _id}) {
         const payload = {username, email, _id};
 
         return jwt.sign({data: payload}, secret, {expiresIn: expiration});
+    }
+
+    authMiddleware: function({req}) {
+        // allows the token to be sent through the req.body, req.query, or the headers
+        let token = req.body.token || req.query.token || req.headers.authorization;
+    
+        // separates "Bearer" from "<tokenvalue>"
+        if (req.headers.authorization) {
+            token = token
+            .split('')
+            .pop()
+            .trim();
+        }
+    
+        // if there isn't a token, return request object as is
+        if (!token) {
+            return req;
+        }
+
+        try {
+            // decode and attach the user data to request object
+            const {data} = jwt.verify(token, secret, {maxAge: expiration});
+            req.user = data;
+        } catch {
+            console.log('Invalid token');
+        }
+
+        // returns the updated request object
     }
 }
