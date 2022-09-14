390f477aa2a5b701f69752c37ae2fe0383316ad1
agvan <agvandyke25@gmail.com>
2022-09-13 19:07:01 -0500

    added the Apollo Provider component to the App.js to pass the client variable in as the value for the client prop

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+
 // ApolloProvider a React component used to provide data to all other components
 // ApolloClient a constructor function helps initialize connection to the GraphQL API server
 // InMemoryCache enables the Apollo Client instance to cache API response data to perform requests more efficietly
@@ -9,13 +10,26 @@ import {
   InMemoryCache,
   createHttpLink,
 } from "@apollo/client";
+
 import Header from "./components/Header";
+
 import Footer from "./components/Footer";
 
 import Home from "./pages/Home";
 
+// establishes connection to back-end server's /graphql endpoint
+const httpLink = createHttpLink({
+  url: 'http://localhost:3001/graphql',
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
+
 function App() {
   return (
+    <ApolloProvider client={client}>
     <div className="flex-column justify-flex-start min-100-vh">
       <Header />
       <div className="container">
@@ -23,6 +37,7 @@ function App() {
       </div>
       <Footer />
     </div>
+    </ApolloProvider>
   );
 }
 
