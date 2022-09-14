db1ed882e655af758274827b891a093e4732c0b0
agvan <agvandyke25@gmail.com>
2022-09-14 12:38:18 -0500

    added useQuery and QUERY_THOUGHTS to the Home() function

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,13 +1,16 @@
-import React from 'react';
+import React from "react";
 // useQuery hook enables queries
-import { useQuery } from '@apollo/client';
-import { QUERY_THOUGHTS } from '../utils/queries';
+import { useQuery } from "@apollo/client";
+import { QUERY_THOUGHTS } from "../utils/queries";
 
 const Home = () => {
+  // use useQuery hook to make query request
+  const { loading, data } = useQuery(QUERY_THOUGHTS);
+  
   return (
     <main>
-      <div className='flex-row justify-space-between'>
-        <div className='col-12 mb-3'>{/* PRINT THOUGHT LIST */}</div>
+      <div className="flex-row justify-space-between">
+        <div className="col-12 mb-3">{/* PRINT THOUGHT LIST */}</div>
       </div>
     </main>
   );
