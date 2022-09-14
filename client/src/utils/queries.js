0d09a2c76fbf7c1ffbcf6792b976aed17f48ef75
agvan <agvandyke25@gmail.com>
2022-09-14 17:39:18 -0500

    corrected the code and ran npm run seeds and app renders correctly now

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,7 +1,7 @@
-import { gql } from "@apollo/client";
+import { gql } from '@apollo/client';
 
 export const QUERY_THOUGHTS = gql`
-  query thougts($username: String) {
+  query thoughts($username: String) {
     thoughts(username: $username) {
       _id
       thoughtText
@@ -19,21 +19,21 @@ export const QUERY_THOUGHTS = gql`
 `;
 
 export const QUERY_THOUGHT = gql`
-query thought($id: ID!) {
-  thought(_id: $id) {
-    -id
-    thoughtText
-    createdAt
-    username
-    reactionCount
-    reactions {
+  query thought($id: ID!) {
+    thought(_id: $id) {
       _id
+      thoughtText
       createdAt
       username
-      reactionBody
+      reactionCount
+      reactions {
+        _id
+        createdAt
+        username
+        reactionBody
+      }
     }
   }
-}
 `;
 
 export const QUERY_USER = gql`
