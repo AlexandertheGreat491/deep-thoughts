234ab0a27b17bea279e96e8922368847d23511ed
agvan <agvandyke25@gmail.com>
2022-09-15 18:16:19 -0500

    declared a handleClick function

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,17 +1,16 @@
-import React from 'react';
-import { Navigate, useParams } from 'react-router-dom';
+import React from "react";
+import { Navigate, useParams } from "react-router-dom";
 // imports the ThoughtList component
-import ThoughtList from '../components/ThoughtList';
+import ThoughtList from "../components/ThoughtList";
 // imports the FriendList component
-import FriendList from '../components/FriendList';
+import FriendList from "../components/FriendList";
 //imports the useQuery & useMutation hooks
-import { useQuery, useMutation } from '@apollo/client';
-import { QUERY_USER, QUERY_ME } from '../utils/queries';
+import { useQuery, useMutation } from "@apollo/client";
+import { QUERY_USER, QUERY_ME } from "../utils/queries";
 // imports AuthorizeService
-import Auth from '../utils/auth';
+import Auth from "../utils/auth";
 // imports ADD_FRIEND mutation
-import {ADD_FRIEND} from '../utils/mutations';
-
+import { ADD_FRIEND } from "../utils/mutations";
 
 const Profile = (props) => {
   const { username: userParam } = useParams();
@@ -43,12 +42,25 @@ const Profile = (props) => {
     );
   }
 
+  const handleClick = async () => {
+    try {
+      await addFriend({
+        variables: {id: user._id}
+      });
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
   return (
     <div>
       <div className="flex-row mb-3">
         <h2 className="bg-dark text-secondary p-3 display-inline-block">
-          Viewing {userParam ? `${user.username}'s` : 'your'} profile.
+          Viewing {userParam ? `${user.username}'s` : "your"} profile.
         </h2>
+        <button className="btn ml-auto" onClick={handleClick}>
+          Add Friend
+        </button>
       </div>
 
       <div className="flex-row justify-space-between mb-3">
