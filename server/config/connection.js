98ad5074f9edf2bf6ab2edc88dc96509fd7abef2
agvan <agvandyke25@gmail.com>
2022-09-15 21:40:29 -0500

    trying to fix deployment issues

--- server/config/connection.js
+++ server/config/connection.js
@@ -3,8 +3,6 @@ const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://Schnauzer1234:Winston1234@cluster0.sexwr.mongodb.net/?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false
 });
 
 module.exports = mongoose.connection;
