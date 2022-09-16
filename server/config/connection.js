548ff6761f27363e4c7561e2da15b8def7f6756f
agvan <agvandyke25@gmail.com>
2022-09-15 21:36:31 -0500

    updated logic

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://Schnauzer1234:Winston123@cluster0.sexwr.mongodb.net/?retryWrites=true&w=majority', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://Schnauzer1234:Winston1234@cluster0.sexwr.mongodb.net/?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
